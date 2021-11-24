4a0525934db18665ad4dcaa3efce3a4bb75851cb
tiamabderezai <pass>
2021-11-23 20:35:40 -0500

    added data package classes and packages

---
+++ src/main/kotlin/com/example/data/MessageDataSourceImp.kt
@@ -0,0 +1,19 @@
+package com.example.data
+
+import com.example.data.model.Message
+import org.litote.kmongo.coroutine.CoroutineDatabase
+
+class MessageDataSourceImp(
+    private val db: CoroutineDatabase
+) : MessageDataSource {
+
+    private val messages = db.getCollection<Message>()
+
+    override suspend fun getAllMessages() = messages.find()
+        .descendingSort(Message::timestamp)
+        .toList()
+
+    override suspend fun insertMessages(message: Message) {
+        messages.insertOne(message)
+    }
+}
